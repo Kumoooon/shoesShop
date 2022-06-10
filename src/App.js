3f92552c5e9bf595aa16353476297c1d269b9dfa
kumoon <jkyhj0073@timmanage.com>
2022-06-10 09:38:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import "./App.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import data from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
+export default function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar bg="dark" variant="dark">
+                <Container>
+                  <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                  <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                  </Nav>
+                </Container>
+              </Navbar>
+
+              <div className="main-bg"></div>
+              <div className="flex justify-around">
+                {shoes.map((a, i) => {
+                  return <Card item={shoes[i]} i={i} />;
+                })}
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail" element={<div>으하하</div>} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
+//상품 컴포넌트화,데이터바인딩,map
+function Card(props) {
+  return (
+    <div className="basis-1/4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+      />
+      <h4>{props.item.title}</h4>
+      <p>{props.item.content}</p>
+    </div>
+  );
+}
